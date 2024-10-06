428907832199226c1ea640f6ca4e61c1a28001d3
Antonio Vázquez Montalban <antuane.vazquez04@gmail.com>
2024-10-05 18:43:50 -0600

    Implementacion del componente login

--- Actividad_Branches/src/App.jsx
+++ Actividad_Branches/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './Login.css'
+import Login from './login/login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +18,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
+      <Login />
       <div className="card">
+
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
--- Actividad_Branches/src/main.jsx
+++ Actividad_Branches/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Login from './login/login';  // Mantén el componente Login
+
+ // Importamos el componente Login
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />  {/* Página de Login */}
+        
+      </Routes>
+    </Router>
   </StrictMode>,
-)
+);
